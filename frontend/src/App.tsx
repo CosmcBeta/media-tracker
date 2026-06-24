4f6338f3686d23c559fbb94877982a09562e1b39
Riley <100080465+CosmcBeta@users.noreply.github.com>
2026-06-23 20:13:02 -0400

    added navbar and general layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import Layout from "./components/Layout";
 import ItemDetailPage from "./pages/ItemDetailPage";
 import ItemsPage from "./pages/ItemsPage";
 import ListDetailPage from "./pages/ListDetailPage";
@@ -8,11 +9,13 @@ import SearchPage from "./pages/SearchPage";
 function App() {
 	return (
 		<Routes>
-			<Route path="/items" element={<ItemsPage />}></Route>
-			<Route path="/items/:id" element={<ItemDetailPage />}></Route>
-			<Route path="/lists" element={<ListsPage />}></Route>
-			<Route path="/lists/:id" element={<ListDetailPage />}></Route>
-			<Route path="/search" element={<SearchPage />}></Route>
+			<Route element={<Layout />}>
+				<Route path="/items" element={<ItemsPage />}></Route>
+				<Route path="/items/:id" element={<ItemDetailPage />}></Route>
+				<Route path="/lists" element={<ListsPage />}></Route>
+				<Route path="/lists/:id" element={<ListDetailPage />}></Route>
+				<Route path="/search" element={<SearchPage />}></Route>
+			</Route>
 		</Routes>
 	);
 }
